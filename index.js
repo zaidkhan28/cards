2be4a5e53575b187ea1d66b56be7b936d3b52614
zaidkhan28 <nk9785057@gmail.com>
2024-09-14 14:40:41 +0530

    weather

---
+++ index.js
@@ -0,0 +1,34 @@
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apikey ="32ea5e5907fd409b8b3867531756c1e0";
+const searchbox = document.querySelector(".field input");
+const searchbtn = document.querySelector(".field button")
+const weathericon= document.querySelector(".weather-icon")
+
+async function clickweather(city){
+    const reponse = await fetch(apiUrl +city+ `&appid=${apikey}`);
+    var data = await reponse.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name
+    document.querySelector(".dig").innerHTML = data.main.temp+"°c"
+    document.querySelector(".par").innerHTML = data.main.humidity+"%"
+    document.querySelector(".speed").innerHTML = data.wind.speed+"Km/ph"
+
+    if(data.weather[0].main=="Clouds"){
+      weathericon.src = "cloud.png"
+    }else if(data.weather[0].main=="Clear"){
+        weathericon.src = "clear.jpeg"
+      }else if(data.weather[0].main=="Rain"){
+        weathericon.src = "rain.png"
+      }else if(data.weather[0].main=="Drizzle"){
+        weathericon.src = "drizzle.png"
+      }else if(data.weather[0].main=="Mist"){
+        weathericon.src = "weather1.jpeg"
+      }
+    
+}
+ 
+searchbtn.addEventListener("click",()=>{
+    clickweather(searchbox.value);
+})
